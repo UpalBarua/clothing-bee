22bbba7f19565eca1b7be641f8eff4389704f08d
UpalBarua <upalbarua52@gmail.com>
2024-12-22 21:00:52 +0600

    add: initial project setup

---
+++ src/app/layout.tsx
@@ -0,0 +1,13 @@
+import "./globals.css";
+
+type RootLayoutProps = {
+  children: React.ReactNode;
+};
+
+export default function RootLayout({ children }: Readonly<RootLayoutProps>) {
+  return (
+    <html lang="en">
+      <body className="antialiased">{children}</body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="text-center py-10 text-3xl">
+      <h1>Clothing Bee</h1>
+    </div>
+  );
+}
